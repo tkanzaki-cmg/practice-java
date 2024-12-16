b2ab708809105dd51bc56c992f3baf095d59705d
神崎 <t_kanzaki@cm-group.co.jp>
2024-12-16 18:21:30 +0900

    使ってないimport削除

--- openai-api/practice-java/src/main/java/practice/java/com/Main.java
+++ openai-api/practice-java/src/main/java/practice/java/com/Main.java
@@ -1,7 +1,6 @@
 package practice.java.com;
 
 import java.io.IOException;
-import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URISyntaxException;
